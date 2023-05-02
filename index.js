644c8ebb89e21a62de41a0d9987756f96299997c
scott-hall7 <scotthall179@gmail.com>
2023-05-02 14:11:52 -0500

    updating to express.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require("express");
+const path = require("path")
+const app = express();
+const port = 3000;
 
-http.createServer(function (req, res) {
-  const q = url.parse(req.url, true);
-  const filename = req.url === "/" ? "index.html" : "." + q.pathname + ".html";
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      const errorData = fs.readFileSync('404.html', 'utf8');
-      res.write(errorData);
-      return res.end();
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
+});
 
+app.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname, '/about.html'))
+});
+
+app.get("/contact-me", function (req, res) {
+  res.sendFile(path.join(__dirname, '/contact-me.html'))
+});
+
+app.use( function(req, res, next) {
+  res.sendFile(path.join(__dirname, '/404.html'))
+});
+
+app.listen(port);
